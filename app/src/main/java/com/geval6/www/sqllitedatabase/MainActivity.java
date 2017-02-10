36a0b0d5f57dc10f88d97bc89ee33d7e363b4740
priyathiruvarasan <priya13thjan@gmail.com>
2017-02-10 13:18:07 +0530

    No

---
+++ app/src/main/java/com/geval6/www/sqllitedatabase/MainActivity.java
@@ -0,0 +1,13 @@
+package com.geval6.www.sqllitedatabase;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
